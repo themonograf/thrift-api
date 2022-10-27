cd752f545eb69c02e0651012faa1746477894312
Fitzgeral <fitzgeralmedia@gmail.com>
2022-10-27 21:17:19 +0700

    add cors

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-require("dotenv").config()
-const express = require('express')
-const expressValidator = require('express-validator')
-const app = express()
-const userRouter = require("./routes/user")
-const authRouter = require("./routes/auth")
-const productCategory = require("./routes/product_category")
-const upload = require("./routes/upload")
+require("dotenv").config();
+const express = require("express");
+const expressValidator = require("express-validator");
+const app = express();
+const cors = require("cors");
+const userRouter = require("./routes/user");
+const authRouter = require("./routes/auth");
+const productCategory = require("./routes/product_category");
+const upload = require("./routes/upload");
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(expressValidator.check())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(expressValidator.check());
+app.use(cors());
 
-app.use("/api/users", userRouter)
-app.use("/api/auth", authRouter)
-app.use("/api/product-category", productCategory)
-app.use("/api/upload", upload)
+app.use("/api/users", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/product-category", productCategory);
+app.use("/api/upload", upload);
 
-app.listen(process.env.APP_PORT,()=>{
-    console.log("server is running")
-})
+app.listen(process.env.APP_PORT, () => {
+  console.log("server is running");
+});
